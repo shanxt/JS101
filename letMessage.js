eb9fe003a6dc0647e86cc62ccec7fe20915599c4
Shashank Shekhar Tewari <shanxt@gmail.com>
2019-02-22 11:12:47 +0530

    Initial JS101 files

---
+++ letMessage.js
@@ -0,0 +1,15 @@
+"use strict";
+
+let message;
+
+message = 'Hello Amit!!';
+
+alert(message);
+
+let age = 12;
+
+let message2 = 'Amit has '+age+' oranges' ;
+
+alert(message2)
+
+
---
+++ variableType.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/*let message;
+
+message = null;
+
+alert(message);
+*/
+
+let num = 2;
+let numz; let numz2; 
+numz = num + num;
+let message2 = 'Amit has '+ numz +' oranges' ;
+alert(message2);
+
+alert(typeof(num));
+
+let text_num = String(num);
+
+numz2 = text_num + text_num ; 
+let message3 = 'Amit has '+ numz2 +' oranges' ;
+alert(message3);
